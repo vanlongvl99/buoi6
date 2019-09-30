bad88f9978d030fc34a04d27e83624ffc88b995a
vanlongvl99 <nguyenvanlongt2@gmail.com>
2019-09-30 21:49:42 +0700

    dudu.py

--- dudu.py
+++ dudu.py
@@ -1,28 +1,23 @@
 t = int(input())
 
 def checkLoops(n, m):
-    visited = [False for i in range(n+1)]
-    graph = [[] for i in range(n+1)]
+    path = [[] for i in range(n+1)] 
+    # graph = [[] for i in range(n+1)]
     for i in range(m):
         a, b = map(int, input().split())
-        graph[a].append(b)
+        # graph[a].append(b)
+        path[b].append(a)
     for i in range(1,n +1):
-        arr = []
-        if visited[i] == False:
-            visited1 = [False for i in range(n+1)]
-            visited[i] = True
-            visited1[i] = True
-            arr.append(i)
-            while len(arr) != 0:
-                k = arr.pop()
-                for x in graph[k]:
-                    if visited1[x] == False:
-                        visited1[x] = True
-                        visited[x] = True
-                        arr.append(x)
-                    else:
-                        return "YES"
+        arr = [i]
+        while len(arr) != 0:
+            k = arr.pop()
+            for x in path[k]:
+                if x == i:
+                    return "YES"
+                arr.append(x)
     return "NO"
+       
+   
                         
 
 
