c2e71f961fcab7c89d37303071afc0887c94bb29
vanlongvl99 <nguyenvanlongt2@gmail.com>
2019-10-01 08:40:18 +0700

    shot.py

--- shot.py
+++ shot.py
@@ -1,28 +1,26 @@
-n, m = map(int, input().split())
-graph = [[] for i in range(n+1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-maxImpact = 0
-visited = [False for i in range(n+1)]
 
-
-def recursion(i, visited, graph,visit1, count):
+def recursion(i, visited, graph,fakeVisited, count):
     count += 1
     for x in graph[i]:
-        if visit1[x] == False:
-            visit1[x] = True
+        if fakeVisited[x] == False:
+            fakeVisited[x] = True
             visited[x] = True
-            count = recursion(x, visited, graph, visit1, count)
+            count = recursion(x, visited, graph, fakeVisited, count)
     return count
-
-for i in range(1, n+1):
-    if visited[i] == False:
-        visited[i] = True
-        visit1 = [False for j in range(n+1)]
-        visit1[i] = True
-        count = 0
-        count = recursion(i, visited, graph, visit1, count)
-        maxImpact = max(maxImpact, count)
-                
-print(maxImpact)
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    graph = [[] for i in range(n+1)]
+    for i in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+    maxImpact = 0
+    visited = [False for i in range(n+1)]
+    for i in range(1, n+1):
+        if visited[i] == False:
+            visited[i] = True
+            fakeVisited = [False for j in range(n+1)]
+            fakeVisited[i] = True
+            count = 0
+            count = recursion(i, visited, graph, fakeVisited, count)
+            maxImpact = max(maxImpact, count)
+    print(maxImpact)
